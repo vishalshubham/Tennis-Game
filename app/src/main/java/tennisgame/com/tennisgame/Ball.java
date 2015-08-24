9c91b23399e9d308039bd7577739558937142dab
vishalshubham <vishal.shubham@gmail.com>
2015-08-24 14:00:00 +0100

    Ball bouncing off the boundaries now

--- app/src/main/java/tennisgame/com/tennisgame/Ball.java
+++ app/src/main/java/tennisgame/com/tennisgame/Ball.java
@@ -1,12 +1,14 @@
 package tennisgame.com.tennisgame;
 
+import android.graphics.Rect;
+
 /**
  * Created by Vishal on 30/08/2015.
  */
 public class Ball extends Sprite{
 
-    private float speedX = 0.5f;
-    private float speedY = 0.5f;
+    private float speedX = 0.05f;
+    private float speedY = 0.05f;
 
     private int directionX = 1;
     private int directionY = 1;
@@ -18,6 +20,22 @@ public class Ball extends Sprite{
     public void update(long elapsed){
         float x = getX();
         float y = getY();
+
+        Rect screenRect = getScreenRect();
+
+        if(screenRect.left <= 0){
+            directionX = 1;
+        }
+        else if(screenRect.right >= getWidth()){
+            directionX = -1;
+        }
+
+        if(screenRect.top <=0){
+            directionY = 1;
+        }
+        else if(screenRect.bottom >= getHeight()){
+            directionY = -1;
+        }
 
         x += directionX * speedX * elapsed;
         y += directionY * speedY * elapsed;
--- app/src/main/java/tennisgame/com/tennisgame/Sprite.java
+++ app/src/main/java/tennisgame/com/tennisgame/Sprite.java
@@ -2,6 +2,7 @@ package tennisgame.com.tennisgame;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 /**
  * Created by Vishal on 29/08/2015.
@@ -14,6 +15,8 @@ public class Sprite {
     public int height;
     private Bitmap image;
 
+    private Rect bounds;
+
     public Sprite(int width, int height){
         this.x = 30;
         this.y = 30;
@@ -23,10 +26,35 @@ public class Sprite {
 
     public void init(Bitmap image){
         this.image = image;
+        bounds = new Rect(0, 0, image.getWidth(), image.getHeight());
     }
 
     public void draw(Canvas canvas){
         canvas.drawBitmap(image, x, y, null);
+    }
+
+    public Rect getRect(){
+        return bounds;
+    }
+
+    public Rect getScreenRect(){
+        return new Rect((int)x, (int)y, (int)x + getRect().width(), (int)y + getRect().height());
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public float getX() {
