44e647835ac9e39aeae47b82fdf62e064109062e
vishalshubham <vishal.shubham@gmail.com>
2015-08-29 20:39:25 -0400

    Started implementation

---
+++ app/src/main/java/tennisgame/com/tennisgame/GameView.java
@@ -0,0 +1,14 @@
+package tennisgame.com.tennisgame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Vishal on 29/08/2015.
+ */
+public class GameView extends SurfaceView {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
